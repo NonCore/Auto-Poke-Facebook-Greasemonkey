59acad16b3737cf8d872dc0fa48377e612848178
NonCore <53410773+NonCore@users.noreply.github.com>
2024-04-05 22:12:42 +0800

    Create Autopoke.js

---
+++ Autopoke.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name        Facebook Poke Back Bot
+// @namespace   fb
+// @description A simple script that automatically pokes back when you receive a poke on Facebook
+// @include     https://www.facebook.com/pokes/?notif_t=poke
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+// Initialize the total number of pokes
+let totalPokes = 0;
+
+// Function to perform the poke back action
+function pokeBack() {
+  // Get all elements on the page
+  const allElements = document.getElementsByTagName("span");
+
+  // Loop through all elements
+  for (let i = 0; i < allElements.length; i++) {
+    // Assign each element to the currentElement variable
+    const currentElement = allElements[i];
+
+    // Check if the element contains the text "Poke Back"
+    if (currentElement.innerHTML.includes("Poke Back")) {
+      // Perform the poke back action
+      currentElement.click();
+
+      // Increment the total number of pokes
+      totalPokes++;
+
+      // Log the total number of pokes to the console
+      console.log(`Total pokes: ${totalPokes / 2}`);
+    }
+  }
+
+  // Call the pokeBack function again after a delay of 1 second
+  setTimeout(pokeBack, 1000);
+
+  // Reload the page after a delay of 2 minutes
+  setTimeout(reloadPage, 120000);
+}
+
+// Function to reload the page
+function reloadPage() {
+  location.reload();
+}
+
+// Call the pokeBack function to start the script
+pokeBack();
